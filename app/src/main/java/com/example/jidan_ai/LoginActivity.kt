e3ceb44cbe1ad4ccd655ae2e3d464cb50a8de802
sajidan03 <rifansyahsajidan03@gmail.com>
2025-07-04 22:52:57 +0700

    Sajidan

---
+++ app/src/main/java/com/example/jidan_ai/LoginActivity.kt
@@ -0,0 +1,26 @@
+package com.example.jidan_ai
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val login = findViewById<AppCompatButton>(R.id.login)
+        login.setOnClickListener {
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+        }
+    }
+}
